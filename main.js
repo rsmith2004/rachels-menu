f7893bc6b8204989f94c3708f28eef57c2b82064
Rachel Smith <rachelsmith2027@u.northwestern.edu>
2025-02-17 16:14:41 -0800

    js that doesn't work

---
+++ main.js
@@ -0,0 +1,24 @@
+const startOrder = document.querySelector("#startOrder")
+const disclaimer = document.querySelector("dislaimer")
+
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let currentTime = hours + ":" + minutes
+
+const AlmostClosed = function(){
+    alert("Warning, we are about to close.")
+}
+
+const WeAreClosed = function(){
+    Disclaimer.innerHTML = "We are Closed. Try ordering tomorrow!"
+}
+
+if (hours === 16 && minutes >= 30 && minutes < 59){
+   startOrder.addEventListener("click", AlmostClosed)
+} else if (hours >= 17) {
+    startOrder.addEventListener("click", WeAreClosed)
+}else{
+    " "
+}
