1a6de136a210ec291e28e753287dd7b3d6142424
Rachel Smith <rachelsmith2027@u.northwestern.edu>
2025-02-17 17:11:05 -0800

    nvm it works lol

--- main.js
+++ main.js
@@ -15,10 +15,14 @@ const WeAreClosed = function(){
     Disclaimer.innerHTML = "We are Closed. Try ordering tomorrow!"
 }
 
+console.log("now", now)
+console.log("hours", hours)
+console.log("minutes", minutes)
+
 if (hours === 16 && minutes >= 30 && minutes < 59){
-   startOrder.addEventListener("click", AlmostClosed)
-} else if (hours >= 17) {
-    startOrder.addEventListener("click", WeAreClosed)
-}else{
-    " "
-}
+  startOrder.addEventListener("click", AlmostClosed)
+ } else if (hours >= 17) {
+     startOrder.addEventListener("click", WeAreClosed)
+    }else{
+          " "
+         }
